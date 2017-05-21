89547de75c905b079d66d4c4591dc8ae6b598fae
Jay Sullivan <jay@identity.pub>
2017-05-20 21:21:02 -0400

    Clean up interpreter

--- interpreter.c
+++ interpreter.c
@@ -1,12 +1,31 @@
 #include "interpreter.h"
 #include <stdio.h>
 
-void execute(struct AstElement* ast)
+void interpret_statements(AstElement* ast)
 {
-	printf("TODO: Execute...\n");
-	if(ast->kind==ekStatements)
-	{
-		printf("statements\n");
-	}
+		execute(ast->data.statements.thisStatement);
+		execute(ast->data.statements.childStatements);
+}
+void interpret_print(AstElement* ast)
+{
+		printf("Print\n");
+}
+void interpret_conditional(AstElement* ast)
+{
+		printf("Conditional\n");
+}
+void interpret_assignment(AstElement* ast)
+{
+		printf("Assignment\n");
 }
 
+void execute(struct AstElement* ast)
+{
+	switch(ast->kind) 
+	{
+		case ekStatements: interpret_statements(ast); break;
+		case ekPrint: interpret_print(ast); break;
+		case ekAssignment: interpret_assignment(ast); break;
+		case ekConditional: interpret_conditional(ast); break;
+	}
+}
