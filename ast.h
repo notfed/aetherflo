4206d39d4e3013a35930e63aff603f76fff1e462
Jay Sullivan <jay@identity.pub>
2017-05-27 19:03:06 -0400

    Update ast.h

--- ast.h
+++ ast.h
@@ -1,65 +1,106 @@
-#ifndef ASTGEN_H
-#define ASTGEN_H
+#include "ast.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
-typedef struct AstElement
+namespace Ast
 {
-    enum {ekEmpty=1, ekId, ekVal, ekOp, ekStatements, ekExpression, ekBoolExpression, ekAssignment, ekConditional, ekPrint} kind;
-    union
+    class Val
     {
-            int val;
-            char* name;
-            char* symbol;
+    private:
+        int val;
+    public:
+        inline Val(int val) {
+            this.val = val;
+        }
+    };
+    
+    class Id
+    {
+    private:
+        char* name;
+    public:
+        inline Id(char *name) {
+            this.name = strdup(name);
+        }
+    };
+    
+    class Op
+    {
+    private:
+        char* symbol;
+    public:
+        inline Id(char *name) {
+            this.symbol = strdup(name);
+        }
+    };
 
-            struct
-            {
-              struct AstElement* thisStatement;
-              struct AstElement* childStatements;
-            } statements;
+    class Expression
+    {
+    private:
+        Expression *left;
+        Expression *right;
+        Op *op;
+    public:
+        inline Expression(Expression *left, Expression *right, Op* op) {
+            this.left = left;
+            this.right = right;
+            this.op = op;
+        }
+        int Evaluate();
+    };
 
-            struct
-            {
-              struct AstElement *left, *right;
-              struct AstElement *op;
-            } expression;
+    class Statement
+    {
+    public:
+        virtual void Execute() = 0;
+    };
 
-            struct
-            {
-              struct AstElement *left, *right;
-              struct AstElement* relop;
-            } boolexpression;
-
-            struct
-            {
-              struct AstElement* name;
-              struct AstElement* right;
-            } assignment;
-
-            struct
-            {
-              struct AstElement* condition;
-              struct AstElement* statement;
-            } conditional;
-
-            struct
-            {
-              struct AstElement* expr;
-            } print;
-
-        } data;
-} AstElement;
-
-void execute(struct AstElement* ast);
-
-struct AstElement* makeEmpty();
-struct AstElement* makeVal(int val);
-struct AstElement* makeId(char *name);
-struct AstElement* makeOp(char *symbol);
-
-struct AstElement* makeStatement(struct AstElement* thistatement, struct AstElement* childStatements);
-struct AstElement* makeExp(struct AstElement* left, struct AstElement* right, struct AstElement* op);
-struct AstElement* makeBoolExp(struct AstElement* left, struct AstElement* relop, struct AstElement* right);
-struct AstElement* makeAssignment(struct AstElement* name, struct AstElement* right);
-struct AstElement* makeConditional(struct AstElement*condition, struct AstElement* statement);
-struct AstElement* makePrint(struct AstElement* expr);
-
-#endif
+    class Statements
+    {
+    private:
+        Statement *thisStatement;
+        Statements *childStatements;
+    public:
+        inline Statements(Statement *thisStatement, Statements *childStatements) {
+            this.thisStatement = thisStatement;
+            this.childStatements = childStatements;
+        }
+    };
+    
+    class Assignment : public Statement
+    {
+    private:
+        Id *name;
+        Expression *right;
+    public:
+        inline Assignment(Id * name, Expression* right) {
+            this.name = name;
+            this.right = right;
+        }
+    };
+    
+    class Conditional : public Statement
+    {
+    private:
+        Expression *condition;
+        Statement *right;
+    public:
+        inline Conditional(Expression* condition, Statement* right) {
+            this.condition = condition;
+            this.right = right;
+        }
+    };
+    
+    class Print : public Statement
+    {
+    private:
+        Expression *expr;
+    public:
+        inline Print(Expression *expr) {
+            this.expr = expr;
+        }
+    };
+    
+}
